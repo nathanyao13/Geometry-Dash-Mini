611f4d3b9891bfb0fbf61de6e08710f7c8b5293e
Nathan Yao <nate.yao@gmail.com>
2023-12-05 10:42:55 -0500

    fixed the jetpack ground

---
+++ spaceship.py
@@ -0,0 +1,7 @@
+class spaceship:
+    def __init__(self, shape, width, centerX, centerY, topY):
+        self.shape = shape
+        self.width = width
+        self.centerX = centerX
+        self.centerY = centerY
+        self.topY = topY
